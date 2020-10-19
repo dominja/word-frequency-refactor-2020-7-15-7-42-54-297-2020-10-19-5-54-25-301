4061e640dbb39caf87802db61232261747999c0c
Janelle <dominja@oocl.com>
2020-10-19 19:01:02 +0800

    [WordFrequency][Janelle][Re-factor]: added exception scenario in test

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -7,55 +7,50 @@ class WordFrequencyGameTest {
     @Test
     void should_get_the_1_when_input_the() {
         //Given
-        String inputStr = "the";
-        String expectResult = "the 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the", "the 1");
     }
 
     @Test
     void should_process_two_words() {
         //Given
-        String inputStr = "the is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the is", "the 1\nis 1");
     }
 
     @Test
     void should_process_two_words_with_special_spaces() {
         //Given
-        String inputStr = "the      is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the      is", "the 1\nis 1");
     }
 
     @Test
     void should_process_two_words_with_special_enter() {
         //Given
-        String inputStr = "the   \n   is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the   \n   is", "the 1\nis 1");
     }
 
     @Test
     void should_process_two_same_words_with_sorted() {
         //Given
-        String inputStr = "the the is";
-        String expectResult = "the 2\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the the is", "the 2\nis 1");
     }
 
     @Test
     void should_process_sorted_with_count_descending() {
         //Given
-        String inputStr = "the is is";
-        String expectResult = "is 2\nthe 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_Input_words_process_to_expected_word("the is is", "is 2\nthe 1");
     }
 
-    private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
+    @Test
+    void should_return_error_message() {
+        //Given
+        validate_Input_words_process_to_expected_word(null, "Calculate Error");
+    }
+
+
+    private void validate_Input_words_process_to_expected_word(String inputString, String expectResult) {
         WordFrequencyGame game = new WordFrequencyGame();
         //When
-        String result = game.getResult(inputStr);
+        String result = game.getResult(inputString);
         //Then
         assertEquals(result, expectResult);
     }
