1e4115f82433cbd40f4436d9d656621b39c23af0
Janelle <dominja@oocl.com>
2020-10-19 17:05:04 +0800

    [WordFrequency][Janelle][Re-factor]: extracted method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -12,24 +12,33 @@ public class WordFrequencyGame {
     public String getResult(String stringOfWords) {
 
         try {
-            List<WordInfo> wordInfoList = calculateFrequency(stringOfWords);
-            List<WordInfo> wordInfos = wordInfoList.stream()
-                    .sorted((wordInfo1, wordInfo2) ->
-                            wordInfo2.getWordCount() - wordInfo1.getWordCount())
-                    .collect(Collectors.toList());
+            List<WordInfo> wordInfoList = addFrequencyToMap(stringOfWords);
+            List<WordInfo> wordInfos = sortMapToDescending(wordInfoList);
             return buildWordInfo(wordInfos);
         } catch (Exception e) {
             return "Calculate Error";
         }
     }
 
-    private List<WordInfo> calculateFrequency(String stringOfWords) {
-        List<String> wordList = Arrays.asList(stringOfWords.split(WHITE_SPACES));
-        Map<String, Long> wordFrequencyList = wordList.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        return wordFrequencyList.keySet().stream().map(word -> new WordInfo(word, wordFrequencyList
-                .get(word).intValue()))
+    private List<WordInfo> sortMapToDescending(List<WordInfo> wordInfoList) {
+        return wordInfoList.stream()
+                .sorted((wordInfo1, wordInfo2) ->
+                        wordInfo2.getWordCount() - wordInfo1.getWordCount())
                 .collect(Collectors.toList());
+    }
+
+    private List<WordInfo> addFrequencyToMap(String stringOfWords) {
+        Map<String, Long> wordFrequencyList = calculateFrequency(stringOfWords);
+        return wordFrequencyList.keySet().stream().map(word ->
+                new WordInfo(word, wordFrequencyList
+                        .get(word).intValue()))
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Long> calculateFrequency(String stringOfWords) {
+        List<String> wordList = Arrays.asList(stringOfWords.split(WHITE_SPACES));
+        return wordList.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
     private String buildWordInfo(List<WordInfo> inputList) {
