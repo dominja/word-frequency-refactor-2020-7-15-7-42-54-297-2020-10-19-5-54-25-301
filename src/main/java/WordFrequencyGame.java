7fcc025bda435e0af8a3bdab41827c8a0fe626ef
Janelle <dominja@oocl.com>
2020-10-19 15:28:45 +0800

    [WordFrequency][Janelle][Re-factor]: renamed generic naming to more detailed and created separate method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -4,37 +4,42 @@ public class WordFrequencyGame {
 
     private static final String WHITE_SPACES = "\\s+";
 
-    public String getResult(String inputStr) {
+    public String getResult(String stringOfWords) {
 
 
-        if (inputStr.split(WHITE_SPACES).length == 1) {
-            return inputStr + " 1";
+        if (stringOfWords.split(WHITE_SPACES).length == 1) {
+            return stringOfWords + " 1";
         } else {
 
             try {
-                String[] words = inputStr.split(WHITE_SPACES);
+                List<WordInfo> wordInfoList = calculateFrequency(stringOfWords);
 
-                List<WordInfo> inputList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo input = new WordInfo(word, 1);
-                    inputList.add(input);
-                }
-
-                Map<String, List<WordInfo>> wordInfoMap = getListMap(inputList);
+                Map<String, List<WordInfo>> wordInfoMap = getListMap(wordInfoList);
 
                 List<WordInfo> wordInfos = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()) {
                     WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
                     wordInfos.add(input);
                 }
-                inputList = wordInfos;
-                inputList.sort((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount());
+                wordInfoList = wordInfos;
+                wordInfoList.sort((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount());
 
-                return buildWordInfo(inputList);
+                return buildWordInfo(wordInfoList);
             } catch (Exception e) {
                 return "Calculate Error";
             }
         }
+    }
+
+    private List<WordInfo> calculateFrequency(String stringOfWords) {
+        String[] words = stringOfWords.split(WHITE_SPACES);
+
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordInfo input = new WordInfo(word, 1);
+            wordInfoList.add(input);
+        }
+        return wordInfoList;
     }
 
     private String buildWordInfo(List<WordInfo> inputList) {
